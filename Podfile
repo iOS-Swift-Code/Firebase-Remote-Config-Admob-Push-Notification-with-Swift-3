fbfb62e65ac8af93467bf8f6aa3e8854c974a51f
Kokpheng <yinkokpheng@yahoo.com>
2016-12-17 13:03:00 +0700

    Init

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'PushNotification' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PushNotification
+pod 'Firebase/RemoteConfig'
+pod 'Firebase/AdMob'
+pod 'Firebase/Messaging'
+end
--- PushNotification/ViewController.swift
+++ PushNotification/ViewController.swift
@@ -8,18 +8,71 @@
 
 import UIKit
 
+import Firebase
+import GoogleMobileAds
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bannerView: GADBannerView!
+    var remoteConfig : FIRRemoteConfig?
+    @IBOutlet var showLabel : UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+       
         // Do any additional setup after loading the view, typically from a nib.
+        // RemoteConfig
+        remoteConfig = FIRRemoteConfig.remoteConfig()
+        let remoteConfigSetting = FIRRemoteConfigSettings(developerModeEnabled: true)
+        remoteConfig?.configSettings = remoteConfigSetting!
+        remoteConfig?.setDefaultsFromPlistFileName("FireSwiftRemoteConfigDefaults")
+        
+        fetchConfiguration()
+        
+        // AdMob
+        print("Google Mobile Ads SDK version : \(GADRequest.sdkVersion())")
+        let request = GADRequest()
+        request.testDevices = [kGADSimulatorID]
+        bannerView.adUnitID = "ca-app-pub-2053250789776715/4075049383"
+        bannerView.rootViewController = self
+        bannerView.load(request)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        if let refreshedToken = FIRInstanceID.instanceID().token() {
+            print("InstanceID token: \(refreshedToken)")
+        }
+        
+        FIRMessaging.messaging().subscribe(toTopic: "/topics/news")
+        NSLog("Subscribed to news topic");
     }
-
+    func fetchConfiguration() {
+        print("Before fetch \(remoteConfig?["sampleURL"].stringValue)")
+        
+        var expirationDuration = 3600
+        if (remoteConfig?.configSettings.isDeveloperModeEnabled)! {
+            expirationDuration = 0
+        }
+        
+        remoteConfig?.fetch(withExpirationDuration: TimeInterval(expirationDuration), completionHandler: { (status, error) in
+            if status == .success {
+                print("Config fetched!")
+                self.remoteConfig?.activateFetched()
+            } else {
+                print("Config not fetched")
+                print("Error \(error!.localizedDescription)")
+            }
+        })
+        
+        print("After fetch \(remoteConfig?["sampleURL"].stringValue)")
+        showLabel.text = remoteConfig?["sampleURL"].stringValue
+    }
 
 }
+
+
+
+
+
+
+
 
